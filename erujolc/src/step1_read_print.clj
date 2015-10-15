0a736ac6585ddfda967c297fa77bcbf46e92023f
Matt Oquist <moquist@majen.net>
2015-10-14 22:17:51 -0400

    [erujolc] Improve MalPrinter protocol docstrings.

--- erujolc/src/step1_read_print.clj
+++ erujolc/src/step1_read_print.clj
@@ -5,8 +5,8 @@
 ;; Printer
 
 (defprotocol MalPrinter
-  (mal-print-string [this] "Return the value of 'this as a string.")
-  (mal-print-list [this] "Return the value of 'this as a string."))
+  (mal-print-string [this] "Return the value of 'this as a string (for atomic types).")
+  (mal-print-list [this] "Return the value of 'this as a string (for collection types)."))
 
 ;; ========================================
 ;; Mal types
