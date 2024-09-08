f5858c097c6f7ab9ec097b582f0c7caa2c5b7c67
Matt Oquist <moquist@majen.net>
2024-09-08 16:18:54 -0400

    step8: add cond (11 test failures remain)

--- impls/erujolc/src/step8_macros.clj
+++ impls/erujolc/src/step8_macros.clj
@@ -381,6 +381,9 @@
   (rep "(def! not (fn* (a) (if a false true)))" env)
   (rep "(def! load-file (fn* (filepath) (eval (read-string (str \"(do \" (slurp filepath) \"\nnil)\")))))" env)
   (rep "(def! atom? (fn* (x) (= \"atom\" (type x))))" env)
+  ;; step 8
+  (rep "(defmacro! cond (fn* (& xs) (if (> (count xs) 0) (list 'if (first xs) (if (> (count xs) 1) (nth xs 1) (throw \"odd number of forms to cond\")) (cons 'cond (rest (rest xs)))))))"
+       env)
   )
 
 (defn -main
