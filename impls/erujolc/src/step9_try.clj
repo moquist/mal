2d628eb4b22a3b650bf4ef25ce1514462ecb0c40
Matt Oquist <moquist@majen.net>
2024-10-12 13:23:46 -0400

    add inc and dec

--- impls/erujolc/src/step9_try.clj
+++ impls/erujolc/src/step9_try.clj
@@ -421,6 +421,8 @@
   ;; step 8
   (rep "(defmacro! cond (fn* (& xs) (if (> (count xs) 0) (list 'if (first xs) (if (> (count xs) 1) (nth xs 1) (throw \"odd number of forms to cond\")) (cons 'cond (rest (rest xs)))))))"
        env)
+  (rep "(def! inc (fn* (a) (+ a 1)))" env)
+  (rep "(def! dec (fn* (a) (- a 1)))" env)
   )
 
 (defn -main
